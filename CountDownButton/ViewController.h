27917316145615064f2207813ac69ada90174cc0
RunerZhang <cj@tan-mb.local>
2017-05-26 13:57:29 +0800

    Initial Commit

---
+++ CountDownButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CountDownButton
+//
+//  Created by cj on 2017/5/26.
+//  Copyright © 2017年 r. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CountDownButton/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CountDownButton
+//
+//  Created by cj on 2017/5/26.
+//  Copyright © 2017年 r. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
